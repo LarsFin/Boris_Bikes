a8a8a81e1639e24302576601889dc1cb8d76fe4c
Tim Williamson <timwilliamson1337@gmail.com>
2017-11-23 16:05:22 +0000

    20. using double

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require "./lib/docking_station"
 describe DockingStation do
+let(:bike) { double(:bike)}
+
+  before do
+allow(bike).to receive_messages(:working? => true, :working= =>true)
+    #allow(bike).to receive(:working=)
+  end
+
+
   it { is_expected.to respond_to :release_bike }
 
   it "Be able to set a new capacity of bikes on initialization, else defaults to 20" do
@@ -9,13 +17,11 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it "bike is docked" do
-    bike = double(:bike)
+  it "bike is docked" do   
     expect(subject.dock(bike)).to eq subject.bikes
   end
 
   it "bike is seen" do
-    bike = double(:bike)
     subject.dock(bike)
     expect(subject.bikes[-1]).to eq bike
   end
@@ -24,18 +30,15 @@ describe DockingStation do
 
   describe '#release_bike' do
     it "should release" do
-      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "releases working bikes" do
-    bike = double(:bike)
     subject.dock(bike)
     expect(subject.release_bike).to be_working
 
-     bike = double(:bike)
-     bike.working = false
+    allow(bike).to receive(:working?) {false}
     subject.dock(bike)
 
     expect {subject.release_bike }.to raise_error("Bike Broken")
@@ -48,18 +51,17 @@ describe DockingStation do
 
   describe '#dock' do
     it "Cannot dock more than capacity" do
-      subject.capacity.times { subject.dock(double(:bike)) }
-      expect { subject.dock(double(:bike)) }.to raise_error("Docking station is full already")
+      subject.capacity.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error("Docking station is full already")
     end
 
     it "Will accept broken bikes" do
-      bike = double(:bike)
       bike.working = false
       expect {subject.dock(bike)}.not_to raise_error
     end
 
     it "will accept an argument that corresponds to whether the bike is working" do
-      expect {subject.dock(double(:bike), false)}.not_to raise_error
+      expect {subject.dock(bike, false)}.not_to raise_error
       expect(subject).to respond_to(:dock).with(2).arguments
     end
 
