be21355cf6c3e05efd31dc79da6508daef91e515
Lars <larsfinlay@googlemail.com>
2017-11-23 14:24:11 +0000

    Chapter 19 doubles in tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,12 +10,12 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it "bike is docked" do
-    bike = Bike.new
+    bike = double(:bike)
     expect(subject.dock(bike)).to eq subject.bikes
   end
 
   it "bike is seen" do
-    bike = Bike.new
+    bike = double(:bike)
     subject.dock(bike)
     expect(subject.bikes[-1]).to eq bike
   end
@@ -24,17 +24,17 @@ describe DockingStation do
 
   describe '#release_bike' do
     it "should release" do
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "releases working bikes" do
-    bike = Bike.new
+    bike = double(:bike)
     subject.dock(bike)
     expect(subject.release_bike).to be_working
 
-     bike = Bike.new
+     bike = double(:bike)
      bike.working = false
     subject.dock(bike)
 
@@ -48,18 +48,18 @@ describe DockingStation do
 
   describe '#dock' do
     it "Cannot dock more than capacity" do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error("Docking station is full already")
+      subject.capacity.times { subject.dock(double(:bike)) }
+      expect { subject.dock(double(:bike)) }.to raise_error("Docking station is full already")
     end
 
     it "Will accept broken bikes" do
-      bike = Bike.new
+      bike = double(:bike)
       bike.working = false
       expect {subject.dock(bike)}.not_to raise_error
     end
 
     it "will accept an argument that corresponds to whether the bike is working" do
-      expect {subject.dock(Bike.new, false)}.not_to raise_error 
+      expect {subject.dock(double(:bike), false)}.not_to raise_error
       expect(subject).to respond_to(:dock).with(2).arguments
     end
 
